ab7737537de352da80ac6eb627c63b614132749c
ABearNamedSunshine <benjaminlangdon@cvuhs.org>
2015-03-24 19:56:04 -0400

    Rounding

--- Communications/Data/Scripts/Communications/CommLink.cs
+++ Communications/Data/Scripts/Communications/CommLink.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Data;
-using System.Reflection.Emit;
+using System.Security.Cryptography.X509Certificates;
 using Sandbox.Common;
 using Sandbox.Common.Components;
 using Sandbox.Common.ObjectBuilders;
@@ -9,97 +8,127 @@ using Sandbox.ModAPI;
 using Sandbox.ModAPI.Ingame;
 using Sandbox.ModAPI.Interfaces;
 using VRageMath;
+using IMyCubeBlock = Sandbox.ModAPI.IMyCubeBlock;
+using IMyCubeGrid = Sandbox.ModAPI.IMyCubeGrid;
 using IMySlimBlock = Sandbox.ModAPI.IMySlimBlock;
+using IMyTerminalBlock = Sandbox.ModAPI.IMyTerminalBlock;
 
 //Basic imports
 
 namespace Communications //teleporter namespace
 {
-    [MyEntityComponentDescriptor(typeof (MyObjectBuilder_TextPanel))] //Type of TextPanel, applies to TextPanels
+    [MyEntityComponentDescriptor(typeof(MyObjectBuilder_TextPanel))] //Type of TextPanel, applies to TextPanels
     public class CommLink : MyGameLogicComponent
-        //class CommLink, calls from game logic, further describes what a CommLink is
+    //class CommLink, calls from game logic, further describes what a CommLink is
     {
-        public bool _isComm; //Is it a communications panel?
+        public List<IMySlimBlock> OreDetectors = new List<IMySlimBlock>(); //create new list of blocks
+        public List<IMySlimBlock> Asteroids = new List<IMySlimBlock>(); //create new list of blocks
+        public List<IMySlimBlock> OreDeposits = new List<IMySlimBlock>(); //create new list of blocks
+        public bool IsComm; //Is it a communications panel?
         private int _mTimer; //timer
         private MyObjectBuilder_EntityBase _objectBuilder;
         //private AntennaManager AM = new AntennaManager();
-        private IMyTextPanel commPanel; //for use later
+        private IMyTextPanel _commPanel; //for use later
         public bool Isactive; //bool determining whether a CommLink works or not
 
         public override void Init(MyObjectBuilder_EntityBase objectBuilder)
-            //initializes object, overwrites original object code making it an communications panel
+        //initializes object, overwrites original object code making it an communications panel
         {
             _objectBuilder = objectBuilder;
-            commPanel = Entity as IMyTextPanel; //says this entity is an IMyTextPanel Called entrance_g
+            _commPanel = Entity as IMyTextPanel; //says this entity is an IMyTextPanel Called entrance_g
 
             Entity.NeedsUpdate |= MyEntityUpdateEnum.EACH_FRAME | MyEntityUpdateEnum.EACH_10TH_FRAME | MyEntityUpdateEnum.EACH_100TH_FRAME;
-                //Door state updated every 10th/ 100th frame
+            //Door state updated every 10th/ 100th frame
 
-            Isactive = true;
         }
 
         public override void UpdateAfterSimulation100()
         {
-           
+
         }
 
         //This is the actual check for teleportation
         public override void UpdateBeforeSimulation10() //rewriting the Comm update stuff, activating every 10 frames
         {
-            var myname = commPanel.DisplayNameText; //create string myname, name of Comm
-          
+            var myname = _commPanel.DisplayNameText; //create string myname, name of Comm
+
             if (!myname.Contains("Comm")) return;
 
-            
+
             if (myname.Contains("Main"))
             {
                 //not done
             }
             if (myname.Contains("Ship"))
             {
-                
+
                 var fullString = "";
-                
-                commPanel.WritePublicText(fullString);
-                commPanel.GetTopMostParent().Physics.UpdateAccelerations();
-                var shipPosition = commPanel.GetTopMostParent().GetPosition().ToString();
-                var shipVelocity = commPanel.GetTopMostParent().Physics.LinearVelocity.ToString();
-                var shipAcceleration = commPanel.GetTopMostParent().Physics.LinearAcceleration.ToString();
-                var shipAngle = commPanel.GetTopMostParent().WorldMatrix.GetOrientation();
-                var shipRotation = commPanel.GetTopMostParent().Physics.AngularVelocity.ToString();
-                var shipRotationAcceleration = commPanel.GetTopMostParent().Physics.AngularAcceleration.ToString();
-                var eulerAngle = new Vector3D(Math.Atan2(shipAngle.M32, shipAngle.M33),
-                        Math.Atan2(-shipAngle.M31, Math.Sqrt( Math.Pow(shipAngle.M32, 2) + Math.Pow(shipAngle.M33, 2))),
-                    Math.Atan2(shipAngle.M21, shipAngle.M11));
-                    fullString = "Ship Pos " + shipPosition + "\n Ship Vel " + shipVelocity + "\n Ship Accel " + shipAcceleration + "\n Ship Angle " + eulerAngle + "\n Ship Rot " + shipRotation + "\n Ship Rot Accel " + shipRotationAcceleration;
 
+                _commPanel.WritePublicText(fullString);
+                _commPanel.GetTopMostParent().Physics.UpdateAccelerations();
 
-                commPanel.WritePublicText(fullString);
-                commPanel.ShowPublicTextOnScreen();
-                commPanel.SetValueFloat("FontSize", 1.0f);
-               
+                var shipPosition = _commPanel.GetTopMostParent().GetPosition();
+                shipPosition = new VRageMath.Vector3D(Math.Round(shipPosition.X, 4), Math.Round(shipPosition.Y, 4), Math.Round(shipPosition.Z, 4));
+
+                var shipVelocity = _commPanel.GetTopMostParent().Physics.LinearVelocity;
+                shipVelocity.X = (float)Math.Round(shipVelocity.X, 4);
+                shipVelocity.Y = (float)Math.Round(shipVelocity.Y, 4);
+                shipVelocity.Z = (float)Math.Round(shipVelocity.Z, 4);
+
+                var shipAcceleration = _commPanel.GetTopMostParent().Physics.LinearAcceleration;
+                shipAcceleration.X = (float)Math.Round(shipAcceleration.X, 4);
+                shipAcceleration.Y = (float)Math.Round(shipAcceleration.Y, 4);
+                shipAcceleration.Z = (float)Math.Round(shipAcceleration.Z, 4);
+
+                var shipAngle = _commPanel.GetTopMostParent().WorldMatrix.GetOrientation();
+
+                var shipRotation = _commPanel.GetTopMostParent().Physics.AngularVelocity;
+                shipRotation.X = (float)Math.Round(shipRotation.X, 4);
+                shipRotation.Y = (float)Math.Round(shipRotation.Y, 4);
+                shipRotation.Z = (float)Math.Round(shipRotation.Z, 4);
+
+                var shipRotationAcceleration = _commPanel.GetTopMostParent().Physics.AngularAcceleration;
+                shipRotationAcceleration.X = (float)Math.Round(shipRotationAcceleration.X, 4);
+                shipRotationAcceleration.Y = (float)Math.Round(shipRotationAcceleration.Y, 4);
+                shipRotationAcceleration.Z = (float)Math.Round(shipRotationAcceleration.Z, 4);
+
+                var radianAngle = new Vector3D(Math.Round(Math.Atan2(shipAngle.M32, shipAngle.M33), 4),
+                        Math.Round(Math.Atan2(-shipAngle.M31, Math.Sqrt(Math.Pow(shipAngle.M32, 2) + Math.Pow(shipAngle.M33, 2))), 4),
+                    Math.Round(Math.Atan2(shipAngle.M21, shipAngle.M11), 4));
+
+                fullString = "Ship Pos " + shipPosition + "\n Ship Vel " + shipVelocity + "\n Ship Accel " + shipAcceleration + "\n Ship Angle "
+                    + radianAngle + "\n Ship Rot " + shipRotation + "\n Ship Rot Accel " + shipRotationAcceleration;
+
+                _commPanel.WritePublicText(fullString);
+                _commPanel.ShowPublicTextOnScreen();
+                _commPanel.SetValueFloat("FontSize", 1.0f);
+
             }
             if (myname.Contains("Port"))
             {
                 //not done
             }
-
-            HashSet<IMyEntity> ships = new HashSet<IMyEntity>();
-            //find all of the ships
-            Sandbox.ModAPI.MyAPIGateway.Entities.GetEntities(ships, (x) => x is Sandbox.ModAPI.IMyCubeGrid) ;
-            int i =0;
-            var turrets = new List<IMyLargeMissileTurret>();
-            // go through ships
-            foreach(var ship in ships)
+            if (myname.Contains("Ore"))
             {
-                var templist = new List<Sandbox.ModAPI.IMySlimBlock>();
-                (ship as Sandbox.ModAPI.IMyCubeGrid).GetBlocks(templist,x => x is IMyLargeMissileTurret && x.FatBlock.IsWorking && !x.IsDestroyed);
-
-                foreach (var temp in templist)
-                {
-                    turrets.Add(temp.FatBlock as IMyLargeMissileTurret);
-                }
+                GetOre();
+                //not done
             }
+        }
+
+        public void GetOre()
+        {
+            var ship = (_commPanel.GetTopMostParent() as IMyCubeGrid);
+            ship.GetBlocks(OreDetectors, x =>
+            {
+                var myTerminalBlock = x.FatBlock as IMyTerminalBlock;
+                return myTerminalBlock != null && (x.FatBlock is IMyOreDetector && this.IsActive(x.FatBlock));
+            });
+
+        }
+
+        public bool IsActive(IMyCubeBlock comm) //checks whether a portal is active or not
+        {
+            return comm.IsWorking || comm.IsFunctional;
         }
 
         public override void UpdateAfterSimulation()
@@ -112,9 +141,7 @@ namespace Communications //teleporter namespace
                     if (man.Teleportplayer(entrance_g, exit_g, player))//no idea what this says
                     {
                         WasUsed = true;//set portal wasused to true
-
                         isactive = false;//turn off isactive, starts cooldown
-
                     }
                 }
             }*/
@@ -122,57 +149,39 @@ namespace Communications //teleporter namespace
         /*public List<Sandbox.ModAPI.IMySlimBlock> GetCommList()//list of Valid Communication textpanels, called up by GetcommList()
         {
             HashSet<IMyEntity> hash = new HashSet<IMyEntity>();//Creates new IMyEntity hash set
-
             List<Sandbox.ModAPI.IMySlimBlock> commList = new List<Sandbox.ModAPI.IMySlimBlock>();//create new list of blocks
-
             Sandbox.ModAPI.MyAPIGateway.Entities.GetEntities(hash, (x) => x is Sandbox.ModAPI.IMyCubeGrid);//puts all cube grids in hash
-
             foreach (var entity in hash)//for each entity in hash
             {
-
                 Sandbox.ModAPI.IMyCubeGrid grid = entity as Sandbox.ModAPI.IMyCubeGrid;//creates grid based around each entity in hash
-
                 try//try this out because if wrong it breaks game
                 {
                     grid.GetBlocks(commList, (x) => x.FatBlock is IMyTextPanel && (x.FatBlock as IMyTerminalBlock).CustomName.Contains("Comm") && man.isActive(x.FatBlock));//Checks if it is an active Comm that contains portal
-
                 }
                 catch// if the try didnt work
                 {
                     MyAPIGateway.Utilities.ShowNotification("Error When trying to find Comms", 250);//say there was an error
                 }
-
             }
-
-
             return commList;
         }//end of commlist creation
-
         //Will rewrite bellow to handle antenna connections
         private IMyTextPanel GetNearestcommOnDifferentGrid(IMyTextPanel sourcecomm)//this is the check for determining nearest active comm in list
         {
             List<Sandbox.ModAPI.IMySlimBlock> commList = GetCommList();//commList is commList from above
-
             double distance = 0.0d;//distance variable, also makes first comm checked in the for each below relative 0
-
             IMyTextPanel nearest = null;//set nearest variable of IMyTextPanel to null
-
             foreach (var comm in commList)//for each comm in commList(every active Comm)
             {
-
                 if (comm.IsDestroyed || !((comm.FatBlock as IMyTextPanel).Enabled)/* I think this means that the Comm is on, not off in settings  || !comm.FatBlock.IsFunctional || (sourcecomm.GetPosition() == comm.FatBlock.GetPosition()))//Skip disabled, or activated or destroyed comms
-
                     continue;//skips current comm in commList, goes onto next comm
                 if ((distance == 0.0d || (sourcecomm.GetPosition() - comm.FatBlock.GetPosition()).Length() < distance) && (sourcecomm.CustomName == (comm.FatBlock as IMyTerminalBlock).CustomName)) //if it is the first comm checked or it is closest comm so far and if the thing has the same name
                 {
                     nearest = comm.FatBlock as IMyTextPanel;//sets this comm as the closest
-
                     distance = (comm.GetPosition() - comm.FatBlock.GetPosition()).Length();//sets distance to length of closest comm
                 }
             }
-
             return nearest;//returns the closest comm checked
-
         }// end of get nearest comm
         */
         //useless stuff, DO NOT DELETE 
